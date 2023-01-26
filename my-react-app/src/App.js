f60a77fa22eda8f6d7f34822a294e61ba34e3b69
Menyus11 <senkialfonz11@gmail.com>
2023-01-26 20:48:35 +0100

    React accessibility 1.0

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -8,6 +8,7 @@ import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
 
 const ThemeContext = createContext();
+const ArrayContext = createContext();
 
 function App() {
 
@@ -23,46 +24,70 @@ function App() {
   }, []);
 
   return (
-    <div className="App" style={{ backgroundColor: "#504949" }}>
-      <ThemeContext.Provider value={isAccessibility}>
+    <ThemeContext.Provider value={isAccessibility}>
+      <div className="App" style={{ backgroundColor: "#2596be" }}>
+
+        <header>
+          <h1 className="text-dark">Változó idézetek, híres emberektől.</h1>
+        </header>
+
         <nav>
-          <ExampleNavbar changeState={setAccessibility} state={isAccessibility} />
+          <ExampleNavbar setAccessibility={setAccessibility} />
         </nav>
+
         <main className='p-3'>
-          <MainExample quotesArray={quotesArray} />
+          <ArrayContext.Provider value={quotesArray}>
+            <MainExample />
+          </ArrayContext.Provider>
         </main>
-      </ThemeContext.Provider>
-    </div>
+
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
-function ExampleNavbar(props) {
+function ExampleNavbar({ setAccessibility }) {
+
+  const accessibility = useContext(ThemeContext);
+
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-
-        <Navbar.Brand href="#home" className='text-warning'>Carbon Cloud Dashboard</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Form className="d-flex mx-5">
+              <Form.Label>
+                <div>
+                  <label htmlFor='inputSearch' className='text-warning fs-4 mx-3'><b>Keresnék!</b></label>
+                </div>
+              </Form.Label>
+
               <Form.Control
+                id='inputSearch'
                 type="search"
-                placeholder="Search"
+                validated="false"
+                placeholder="Keresés"
                 className="me-2 bg-warning"
                 aria-label="Search"
               />
+
               <Button variant="outline-warning">Search</Button>
             </Form>
           </Nav>
           <Nav>
             <Nav.Link>
               <i className="fa-solid fa-pen fa-lg text-warning mx-2"></i>
-              <i className="fa-solid fa-bell fa-lg text-warning mx-2"></i>
-              <i className="fa-solid fa-user fa-lg text-warning mx-2"></i>
-              <i className="fa-solid fa-wheelchair fa-lg text-warning mx-2" onClick={() => { props.changeState(!props.state) }}></i>
             </Nav.Link>
-
+            <Nav.Link>
+              <i className="fa-solid fa-bell fa-lg text-warning mx-2"></i>
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fa-solid fa-user fa-lg text-warning mx-2"></i>
+            </Nav.Link>
+            <Nav.Link tabIndex={1}>
+              <i className="fa-solid fa-wheelchair fa-lg text-warning mx-2" onClick={() => { setAccessibility(!accessibility) }}></i>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -70,29 +95,31 @@ function ExampleNavbar(props) {
   );
 }
 
-function MainExample(props) {
+function MainExample() {
 
   const accessibility = useContext(ThemeContext);
+  const quotesArrayNow = useContext(ArrayContext);
 
   let quotesNow = [];
 
- 
-    for (let i = 0; i < 20; i++) {
-      quotesNow.push(props.quotesArray[Math.floor(Math.random() * props.quotesArray.length)]);
+  for (let i = 0; i < 20; i++) {
+    quotesNow.push(quotesArrayNow[Math.floor(Math.random() * quotesArrayNow.length)]);
   }
 
   return (
     <div className=' d-flex flex-wrap justify-content-around'>
       {quotesNow.map((quoteNow, index) =>
-        <div className='m-2' key={index}>
-          <Card style={{ height: '12rem', width: quoteNow?.text.length < 100 ? '20rem' : '30rem'}}>
-            <Card.Body className={accessibility ? "bg-dark" : "bg-light"}>
-              <Card.Title className={accessibility ? "text-warning" : "text-dark"}>{quoteNow?.author === null ? "Ismeretlen szerző": quoteNow?.author}</Card.Title>
+        <div className='my-3' key={index}>
+          <Card style={{ height: '12rem', width: quoteNow?.text.length < 100 ? '20rem' : '30rem' }} className="rounded-5">
+            <Card.Body className={accessibility ? "rounded-5 bg-dark" : "rounded-5 bg-light"}>
+              <Card.Title className={accessibility ? "text-warning" : "text-dark"}>{quoteNow?.author === null ? "Ismeretlen szerző" : quoteNow?.author}</Card.Title>
               <Card.Text className={accessibility ? "text-warning" : "text-dark"}>
                 {quoteNow?.text}
               </Card.Text>
               <div className='d-flex justify-content-start'>
-                <Button className={accessibility ? "btn-warning" : "btn-primary"} >Érdekel<i className="fa-solid fa-arrow-right px-1"></i></Button>
+                <a href='https://pakwired.com/100-best-quotes-time'>
+                  <Button className={accessibility ? "btn-warning" : "btn-primary"} >Érdekel<i className="fa-solid fa-arrow-right px-1"></i></Button>
+                </a>
               </div>
             </Card.Body>
           </Card>
@@ -101,7 +128,5 @@ function MainExample(props) {
     </div>
   )
 }
-
-
 
 export default App;
