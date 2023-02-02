28ab8f0102ca8538a0bf5aa0b6db70f66cec60e9
Menyus11 <senkialfonz11@gmail.com>
2023-02-02 12:54:39 +0100

    React learning

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import { useState, createContext, useContext, useEffect } from 'react'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+/* import Button from 'react-bootstrap/Button'; */
 import Card from 'react-bootstrap/Card';
+import ExampleNavbar from './components/navbar';
 
 const ThemeContext = createContext();
 const ArrayContext = createContext();
+export {ThemeContext};
 
 function App() {
 
@@ -22,7 +20,7 @@ function App() {
     fetch("https://type.fit/api/quotes")
       .then((res) => res.json())
       .then((json) => {
-        for (let i = 0; i < 20; i++) {
+        for (let i = 0; i < 30; i++) {
           randomArrayNow.push(json[Math.floor(Math.random() * json.length)]);
         }
         setQuotesArray(randomArrayNow);
@@ -31,7 +29,7 @@ function App() {
 
   return (
     <ThemeContext.Provider value={isAccessibility}>
-      <div className="App" style={{ backgroundColor: "#2596be" }}>
+      <div className="App container-fluid" style={{ backgroundColor: "#2596be" }}>
         <header>
           <h1 className="text-dark">Változó idézetek, híres emberektől.</h1>
         </header>
@@ -48,76 +46,25 @@ function App() {
   );
 }
 
-function ExampleNavbar({ setAccessibility }) {
-
-  const accessibility = useContext(ThemeContext);
-
-  return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Form className="d-flex mx-5">
-
-              <Form.Label>
-                <div>
-                  <label htmlFor='inputSearch' className='text-warning fs-4 mx-3'><b>Keresnék!</b></label>
-                </div>
-              </Form.Label>
-
-              <Form.Control
-                id='inputSearch'
-                type="search"
-                validated="false"
-                placeholder="Keresés"
-                className="me-2 bg-warning"
-                aria-label="Search"
-              />
-
-              <Button variant="outline-warning">Search</Button>
-            </Form>
-          </Nav>
-          <Nav>
-            <Nav.Link>
-              <i className="fa-solid fa-pen fa-lg text-warning mx-2"></i>
-            </Nav.Link>
-
-            <Nav.Link>
-              <i className="fa-solid fa-bell fa-lg text-warning mx-2"></i>
-            </Nav.Link>
-            <Nav.Link>
-              <i className="fa-solid fa-user fa-lg text-warning mx-2"></i>
-            </Nav.Link>
-            <Nav.Link>
-              <i className="fa-solid fa-wheelchair fa-lg text-warning mx-2" onClick={() => { setAccessibility(!accessibility) }}></i>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-}
-
 function MainExample() {
   const accessibility = useContext(ThemeContext);
   const quotesArray = useContext(ArrayContext);
 
   return (
-    <div className=' d-flex flex-wrap justify-content-around'>
+    <div className=' d-flex flex-wrap justify-content-evenly'>
       {quotesArray.map((quoteNow, index) =>
         <div className='my-3' key={index}>
-          <Card style={{ height: '12rem', width: quoteNow?.text.length < 100 ? '20rem' : '30rem' }} className="rounded-5">
-            <Card.Body className={accessibility ? "rounded-5 bg-dark" : "rounded-5 bg-light"}>
-              <Card.Title className={accessibility ? "text-warning" : "text-dark"}>{quoteNow?.author === null ? "Ismeretlen szerző" : quoteNow?.author}</Card.Title>
-              <Card.Text className={accessibility ? "text-warning" : "text-dark"}>
+          <Card style={{ height: '10rem', width: quoteNow?.text.length < 100 ? '20rem' : '30rem' }} className="rounded-2">
+            <Card.Body className={accessibility ? "rounded-2 bg-dark" : "rounded-2 bg-light"}>
+              <Card.Title className={accessibility ? "text-warning pb-3" : "text-dark pb-3"}>{quoteNow?.author === null ? "Ismeretlen szerző" : quoteNow?.author}</Card.Title>
+              <Card.Text className={accessibility ? "text-warning " : "text-dark"}>
                 {quoteNow?.text}
               </Card.Text>
-              <div className='d-flex justify-content-start'>
+{/*               <div className='d-flex justify-content-start'>
                 <a href='https://pakwired.com/100-best-quotes-time'>
                   <Button className={accessibility ? "btn-warning" : "btn-primary"} >Érdekel<i className="fa-solid fa-arrow-right px-1"></i></Button>
                 </a>
-              </div>
+              </div> */}
             </Card.Body>
           </Card>
         </div>
